1720b0acbd36e72cd9ab8a540a04bc732d9cbc48
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-10-21 11:10:08 +0500

    ...

--- app/src/main/java/com/jsoft/bankingapp/CurrenciesSection.kt
+++ app/src/main/java/com/jsoft/bankingapp/CurrenciesSection.kt
@@ -44,6 +44,8 @@ import androidx.compose.ui.unit.sp
 import com.jsoft.bankingapp.data.Currency
 import com.jsoft.bankingapp.ui.theme.GreenStart
 
+
+
 val currencyList = listOf(
     Currency(
         name = "USD",
