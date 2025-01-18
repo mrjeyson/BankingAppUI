d1c62118dcca420539eebe4a7252ffb94dbe2ebd
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2025-01-18 12:50:31 +0500

    ...

--- app/src/main/java/com/jsoft/bankingapp/FinanceSection.kt
+++ app/src/main/java/com/jsoft/bankingapp/FinanceSection.kt
@@ -72,7 +72,6 @@ fun FinanceSection() {
 @Composable
 fun FinanceItem(index: Int) {
 
-
     val finance = financeList[index]
     var lastPaddingEnd = 0.dp
     if (index == financeList.size - 1) {
