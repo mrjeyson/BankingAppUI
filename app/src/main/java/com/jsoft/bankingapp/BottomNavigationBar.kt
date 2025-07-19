bfb7d7dc24648769754882cf96bfdb0f73c7332f
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-07-19 12:41:01 +0500

    ...

--- app/src/main/java/com/jsoft/bankingapp/BottomNavigationBar.kt
+++ app/src/main/java/com/jsoft/bankingapp/BottomNavigationBar.kt
@@ -32,7 +32,6 @@ val items = listOf(
 
 
 
-
     BottomNavigationData(title = "Home", icon = Icons.Rounded.Home),
     BottomNavigationData(title = "Wallet", icon = Icons.Rounded.Wallet),
     BottomNavigationData(title = "Notification", icon = Icons.Rounded.Notifications),
